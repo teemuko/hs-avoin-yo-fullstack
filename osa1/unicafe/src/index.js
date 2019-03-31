58fd47a08d4cbbdee6bb73d99786541c0a3b933c
Teemu Kokkinen <kokkinen.teemu@gmail.com>
2019-03-31 18:57:34 +0300

    Osa1 -harjoitus 1.7 unicafe.

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -14,21 +14,39 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const labelGood = 'hyvä'
   const labelNeutral = 'neutraali'
   const labelBad = 'huono'
+  const labelTotal = 'yhteensä'
+  const labelAverage = 'keskiarvo'
+  const labelPositive = 'positiivisia'
 
   const handleGoodClick = () => {
     setGood(good + 1)
+
+    setTotal(total + 1)
+    setAverage( (good + 1 - bad) / (total + 1) )
+    setPositive( ((good + 1) / (total + 1)) * 100 )
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
+
+    setTotal(total + 1)
+    setAverage( (good - bad) / (total + 1) )
+    setPositive( (good / (total + 1)) * 100 )
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
+
+    setTotal(total + 1)
+    setAverage( (good - bad - 1) / (total + 1) )
+    setPositive( (good / (total + 1)) * 100 )
   }
 
   return (
@@ -50,6 +68,9 @@ const App = () => {
         <p>{labelGood} {good}</p>
         <p>{labelNeutral} {neutral}</p>
         <p>{labelBad} {bad}</p>
+        <p>{labelTotal} {total}</p>
+        <p>{labelAverage} {average}</p>
+        <p>{labelPositive} {positive} %</p>
       </div>
     </div>
   )

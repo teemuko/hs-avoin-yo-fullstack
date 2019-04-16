791c1ee7869b9499a66b99ae5fdb554f6d0b3354
Teemu Kokkinen <kokkinen.teemu@gmail.com>
2019-04-16 19:27:02 +0300

    Osa2 -harjoitus 2.16 puhelinluettelo.

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons.js'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import PersonList from './components/PersonList'
@@ -11,12 +11,10 @@ const App = () => {
   const [ filter, setFilter ] = useState('')
 
   const hook = () => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }
 
@@ -51,8 +49,12 @@ const App = () => {
       number: newNumber
     }
 
-    setPersons(persons.concat(personObject))
-    clearInputFields()
+    personService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        clearInputFields()
+      })
   }
 
   const handleNameChange = (event) => {
